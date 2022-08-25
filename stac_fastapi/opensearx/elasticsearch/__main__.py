5073a30717b6443dc874da5610093baa0f750068
Justus Magin <keewis@posteo.de>
2022-08-25 16:37:44 +0200

    skeleton of the elasticsearch API

---
+++ stac_fastapi/opensearx/elasticsearch/__main__.py
@@ -0,0 +1,44 @@
+import argparse
+
+from .app import create_api
+from .dialects import dialects
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--credentials",
+    type=argparse.FileType("r"),
+    help="credentials for the elasticsearch database",
+    required=True,
+)
+parser.add_argument(
+    "--dialect",
+    choices=sorted(dialects),
+    default="ifremer",
+    help="dialect of the elasticsearch database",
+)
+parser.add_argument(
+    "--host", default="127.0.0.1", help="address of the new stac server"
+)
+parser.add_argument(
+    "--port", default=9588, type=int, help="port of the new stac server"
+)
+parser.add_argument("--log-level", default="info", help="verbosity of the server")
+
+args = parser.parse_args()
+
+api = create_api(
+    credentials=args.credentials, dialect=args.dialect, host=args.host, port=args.port
+)
+# app is used by uvicorn
+app = api.app
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(
+        "opensearx.webapi.__main__:app",
+        host=args.host,
+        port=args.port,
+        log_level=args.log_level,
+        reload=True,
+    )
---
+++ stac_fastapi/opensearx/elasticsearch/core.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+from typing import List, Optional, Union
+
+import attrs
+from elasticsearch import RequestsHttpConnection
+from elasticsearch_dsl import connections
+from stac_fastapi.types import stac as stac_types
+from stac_fastapi.types.core import BaseCoreClient, NumType
+from stac_fastapi.types.search import BaseSearchGetRequest, BaseSearchPostRequest
+
+from .dialects import dialects
+
+
+# TODO: make this an async client once elasticsearch-dsl supports async
+@attrs.define
+class ElasticsearchClient(BaseCoreClient):
+    credentials = attrs.field()
+    timeout = attrs.field(
+        default=20, validator=[attrs.validators.instanceof(int), attrs.validators.ge(0)]
+    )
+    dialect = attrs.field(default="ifremer", validator=attrs.validators.in_(dialects))
+
+    session = attrs.field(default=None, init=False)
+
+    def __attrs_post_init__(self):
+        self.session = connections.create_connection(
+            hosts=[self.credentials],
+            timeout=self.timeout,
+            connection_class=RequestsHttpConnection,
+        )
+
+    def close(self):
+        self.session.close()
+        self.session = None
+
+    def all_collections(self, **kwargs) -> stac_types.Collections:
+        pass
+
+    def get_collection(self, collection_id: str, **kwargs) -> stac_types.Collection:
+        pass
+
+    def get_item(self, item_id: str, collection_id: str, **kwargs) -> stac_types.Item:
+        pass
+
+    def item_collection(
+        self, collection_id: str, limit: int = 10, token: str = None, **kwargs
+    ) -> stac_types.ItemCollection:
+        pass
+
+    def get_search(
+        self,
+        collections: Optional[List[str]] = None,
+        ids: Optional[List[str]] = None,
+        bbox: Optional[List[NumType]] = None,
+        datetime: Optional[Union[str, datetime]] = None,
+        limit: Optional[int] = 10,
+        query: Optional[str] = None,
+        token: Optional[str] = None,
+        fields: Optional[List[str]] = None,
+        sortby: Optional[str] = None,
+        **kwargs,
+    ) -> stac_types.ItemCollection:
+        search_request = BaseSearchGetRequest(
+            collections=collections,
+            ids=ids,
+            bbox=bbox,
+            datetime=datetime,
+        )
+        search_request  # stub
+        return None
+
+    def post_search(
+        self, search_request: BaseSearchPostRequest, **kwargs
+    ) -> stac_types.ItemCollection:
+        pass
